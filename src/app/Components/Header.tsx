660732149bbc06c0e8045976251843ef4ac14f91
Bruno Santos Silva <jkbrunowk@hotmail.com>
2025-09-20 22:51:38 -0300

    Home atualizada e responsiva.

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,15 +1,18 @@
 export const Header = () => {
     return (
-        <section className="w-full py-3 px-40 bg-[#1b1b1b] fixed top-0 border-b-4 border-[#e1e1e178] flex items-center justify-between">
-            <div className="text-2xl font-sans">
+        <section className="w-full py-3 md:px-40 px-5 bg-[#1b1b1b] fixed z-50 top-0 border-b-4 border-[#e1e1e178] flex items-center justify-between">
+            <div className="text-2xl font-sans cursor-pointer">
                 VisagismWeb
             </div>
             <nav className="flex gap-8 justify-center items-center text-[17px]">
-                <span className="cursor-pointer transition-all duration-300 hover:scale-110">Home</span>
-                <span className="cursor-pointer transition-all duration-300 hover:scale-110">Services</span>
-                <span className="cursor-pointer transition-all duration-300 hover:scale-110">Gallery</span>
-                <span className="cursor-pointer transition-all duration-300 hover:scale-110">About</span>
-                <span className="cursor-pointer transition-all duration-300 hover:scale-110">Contact</span>
+                <div className="md:flex gap-6 hidden">
+                    <span className="cursor-pointer transition-all duration-300 hover:scale-110">Home</span>
+                    <span className="cursor-pointer transition-all duration-300 hover:scale-110">Services</span>
+                    <span className="cursor-pointer transition-all duration-300 hover:scale-110">Gallery</span>
+                    <span className="cursor-pointer transition-all duration-300 hover:scale-110">About</span>
+                    <span className="cursor-pointer transition-all duration-300 hover:scale-110">Contact</span>
+                </div>
+                
                 <button className="px-4 py-2 cursor-pointer bg-gradient-to-r from-amber-500 to-amber-300 rounded-md text-black font-bold hover:scale-105 transition-all duration-300">
                     Book Now
                 </button>
--- src/app/Components/Home/Home.tsx
+++ src/app/Components/Home/Home.tsx
@@ -1,17 +1,25 @@
 export const Home = () => {
     return (
-        <section className="h-screen flex flex-col items-center justify-center">
-            <h1 className="text-8xl font-bold m-10">Experience Timeless Beauty</h1>
-            <p className="w-5xl text-2xl text-center">
-                Discover a sanctuary where artistry meets rejuvenation. We craft bespoke experiences designed to enhance your natural radiance and leave you feeling uniquely you.
-            </p>
-            <div className="flex gap-6 m-15">
-                <button className="text-[19px] font-extrabold px-5 py-4 cursor-pointer bg-gradient-to-r from-amber-500 to-amber-300 rounded-md text-black  hover:scale-105 transition-all duration-500">
-                    Explore Services
-                </button>
-                <button className="text-[19px] border-2 border-white rounded-md font-extrabold px-5 py-4 cursor-pointer text-white  hover:scale-105 transition-all duration-500">
-                    Book a Consultation
-                </button>
+        <section className="w-full h-screen flex flex-col items-center justify-center overflow-x-auto">
+            <div className="w-full h-full flex justify-center items-center flex-col relative overflow-x-hidden">
+                <div className="bg-black/40 bg-gradient-to-t from-black/85 to-transparent w-full h-full absolute z-10"></div>
+                <img
+                className="w-full h-full absolute z-0 object-cover object-top"
+                src="/Images/Home.jpg" alt="" 
+                />
+                
+                <h1 className="md:text-8xl text-4xl font-bold text-center md:m-10 m-5 z-20 cursor-pointer">Experience Timeless Beauty</h1>
+                <p className="md:w-5xl md:text-2xl w-auto mx-2 text-center  z-20 cursor-pointer">
+                    Discover a sanctuary where artistry meets rejuvenation. We craft bespoke experiences designed to enhance your natural radiance and leave you feeling uniquely you.
+                </p>
+                <div className="flex md:flex-row flex-col gap-6 m-15 z-20">
+                    <button className="md:text-[19px] font-extrabold px-5 py-4 cursor-pointer bg-gradient-to-r from-amber-500 to-amber-300 rounded-md text-black  hover:scale-105 transition-all duration-500">
+                        Explore Services
+                    </button>
+                    <button className="md:text-[19px] border-2 border-white rounded-md font-extrabold px-5 py-4 cursor-pointer text-white  hover:scale-105 transition-all duration-500">
+                        Book a Consultation
+                    </button>
+                </div>
             </div>
         </section>
     )
